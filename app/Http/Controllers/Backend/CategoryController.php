45c82036b96bde76dedc25644f0fe4169c5a8ddf
yarno <minejarno@gmail.com>
2017-09-18 13:14:27 +0200

    namespace fix

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 use App\Category;
 use Session;
 
--- app/Http/Controllers/Backend/SectionsController.php
+++ app/Http/Controllers/Backend/SectionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Section;
 use Session;
 use App\Page;
